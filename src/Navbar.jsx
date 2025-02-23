3df61ebda4fc206c211f84f10e7fb6966769dc17
abhishikthpolumuri@gmail.com <abhishikthpolumuri@gmail.com>
2025-02-23 20:58:11 +0530

    Added Title, still needs refinements

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,17 +4,17 @@ import { motion } from "framer-motion";
 const Navbar = () => {
 
     return (
-        <motion.nav className="h-15 border-3 border-black bg-gray-400 flex flex-row justify-between py-2 px-4
+        <motion.nav className="h-15 bg-#10175a flex flex-row justify-between py-2 px-4 text-white shadow-2xl
         text-2xl" initial = {{y: -100 , opacity: 0}}
                   animate = {{y: 0 , opacity: 1}}
-                  transition = {{duration: 0.6, ease: "easeOut"}}>
+                  transition = {{duration: 1, ease: "easeOut"}}>
             <div className='flex flex-row'>
                     <motion.div animate = {{ rotate: 360 }} transition={{ delay: 0.4, repeat:1 , duration: 0.8}}>
-                        <Aperture size={38}/>
+                        <Aperture size={36}/>
                     </motion.div>
                     <motion.div initial = {{x: -100 , opacitiy: 0}} animate = {{x: 0, opacity: 1}} 
                     transition={{delay: 1.3 , duration: 0.5, type:'spring', bounce: 0.5}}>
-                        <SendHorizontal size={38}/>
+                        <SendHorizontal size={36}/>
                     </motion.div>
             </div>
 
